873521855aa9ea84eff30b094f3304fe65b8ddee
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-12-05 08:00:14 -0300

    Create ApiExternaException.java

---
+++ demoST/src/main/java/com/example/demoST/ApiExternaException.java
@@ -0,0 +1,8 @@
+package com.example.demoST;
+
+public class ApiExternaException extends Exception {
+
+    public ApiExternaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
